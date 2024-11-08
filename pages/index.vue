e2d2ba0a91e0e273901d56b03eaed8f3c5ec6d8a
ArtemMartynow <artem1435@i.ua>
2024-11-08 13:37:59 +0100

    created a home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    
+  <div class="home">
+    <h1>Welcome to your personal diary!</h1>
+    <p>
+      Record your thoughts, ideas and memories. 
+      Sign up to create your first diary or log 
+      in to continue with your existing notes. 
+      Manage your notes, edit and save important 
+      moments in one place!
+    </p>
+    <div class="home__links">
+      <NuxtLink to="/">Log in</NuxtLink>
+      <NuxtLink to="/">Sign up</NuxtLink>
+    </div>
   </div>
 </template>
 
