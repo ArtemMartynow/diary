edaa7efd457b6f5aa36343bb06c537e5c3beca63
ArtemMartynow <artem1435@i.ua>
2024-11-08 20:19:46 +0100

    created login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h1>login</h1>
+  <div class="login">
+    <div class="login__content">
+      <div class="login__title">
+        <h1>Login</h1>
+      </div>
+      <div class="login__inputs">
+        <input type="text" placeholder="Login">
+        <input type="password" placeholder="Password">
+      </div>
+      <div class="login__buttons">
+        <button>Login</button>
+        <span>or</span>
+        <NuxtLink to="/auth/register">Sign up</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
